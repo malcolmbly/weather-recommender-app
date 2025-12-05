b9c730a3c247266abed11c47a5b106dca7a34172
malcolmbly <malcolmhbailey@gmail.com>
2025-12-05 16:25:50 -0700

    updated simplecov config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,13 @@
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 #
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  minimum_coverage 80
+
+  add_filter 'app/admin'
+  add_filter 'config/'
+  add_filter 'db/'
+end
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
